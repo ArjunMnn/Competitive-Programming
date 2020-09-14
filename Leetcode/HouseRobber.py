ee7336a46330ee613ad1fa5ecb197f20af4f74f0
Arjun Menon <arjun7mnn7@gmail.com>
2020-09-14 15:03:04 +0530

    leetcode

---
+++ Leetcode/HouseRobber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        sum1 = 0
+        sum2 = 0
+        for i in nums:
+            sum1,sum2 = sum2 , max(sum1 + i,sum2)
+        
+        return sum2
