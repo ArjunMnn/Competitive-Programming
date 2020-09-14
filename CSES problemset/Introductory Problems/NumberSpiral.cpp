ac76d886ee54be25e438b7c6ee17bf3f400ba73f
Arjun Menon <arjun7mnn7@gmail.com>
2020-09-14 20:36:44 +0530

    cses

---
+++ CSES problemset/Introductory Problems/NumberSpiral.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+// using namespace std;
+// #include <bits/stdc++.h> 
+#include<unordered_set>
+#include<vector>
+#define ll long long int
+#include<unordered_map>
+#include<algorithm>
+#include <cstring>
+#include <cmath>
+using namespace std;
+int main(){
+	
+	int t;
+	cin >> t;
+	while(t--){
+		ll x,y;
+		cin >> x >> y;
+		if (y>x){
+			ll ans;
+			if(y%2){
+				ans = ((y)*(y)) - x + 1;
+			}
+			else{
+				ans = (y-1)*(y-1)+x;
+			}
+			cout << ans << endl;
+		}
+		else{
+			ll ans;
+			if (x%2)
+				ans = (x-1)*(x-1) + y;
+			else
+				ans = (x*x) -y+1;
+			cout << ans << endl;
+		}
+		
+
+	}
+
+	
+
+
+}
+
+
