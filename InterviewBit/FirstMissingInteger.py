289124f612e834ca1b485a65a743580658afdc94
Arjun Menon <arjun7mnn7@gmail.com>
2020-10-11 20:01:06 +0530

    interviewbit

---
+++ InterviewBit/FirstMissingInteger.py
@@ -0,0 +1,11 @@
+def firstMissingPositive(self, A):
+        A = list(filter(lambda x : x > 0, A))
+        A = [len(A) + 2] + A
+        for i, num in enumerate(A):
+            num = abs(num)
+            if num < len(A):
+                A[num] = - abs(A[num])
+        for i in range(1, len(A)):
+            if A[i] > 0:
+                return i
+        return len(A)
